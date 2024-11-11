101b7ee4b17143c34f25e2a9a75b47bdd9e29605
rrheon <yongheon0806@gmail.com>
2024-11-12 00:31:01 +0900

    Naver LoginScene 구현 중 - 아이디 비밀번호 입력 UI 구현 중

--- NaverLogin/Component/InputIDAndPasswordComponent.swift
+++ NaverLogin/Component/InputIDAndPasswordComponent.swift
@@ -8,9 +8,28 @@
 import UIKit
 
 final class InputIDAndPasswordComponent: UIView {
+  @IBOutlet weak var idTextField: UITextField!
+  @IBOutlet weak var idTextFieldPlaceHolderLabel: UILabel!
+  @IBOutlet weak var idTextFieldPlaceHolderTopAnchor: NSLayoutConstraint!
+  
+  @IBOutlet weak var textFieldUnderLine: UIView!
+  
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var passwordTextFieldPlaceHolderLabel: UILabel!
+  @IBOutlet weak var passwordTextFieldPlaceHolderTopAnchor: NSLayoutConstraint!
+  
   override func awakeFromNib() {
     super.awakeFromNib()
     applyNib()
+    
+    idTextField.setupTextFieldCornerRadius(leftCorner: .layerMinXMinYCorner, rightCorner: .layerMaxXMinYCorner)
+    passwordTextField.setupTextFieldCornerRadius(leftCorner: .layerMinXMaxYCorner, rightCorner: .layerMaxXMaxYCorner)
+    
+    idTextField.delegate = self
+    passwordTextField.delegate = self
+    textFieldUnderLine.backgroundColor = .systemGray6
+    
+    idTextField.clearButtonMode = .whileEditing
   }
   
   required override init(frame: CGRect) {
@@ -19,7 +38,76 @@ final class InputIDAndPasswordComponent: UIView {
   }
   
   required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
+    super.init(coder: coder)
   }
   
 }
+
+extension InputIDAndPasswordComponent: UITextFieldDelegate {
+  
+  /// textField 입력 시 placeHolder Label , textFieldBorder 세팅
+  /// - Parameter textField: 해당 textField
+  func textFieldDidBeginEditing(_ textField: UITextField) {
+    
+    updateTextFieldBorder(textField)
+    var placeHolderLabel: UILabel? = nil
+    var topAnchor: NSLayoutConstraint? = nil
+    
+    placeHolderLabel = textField == idTextField ? idTextFieldPlaceHolderLabel : passwordTextFieldPlaceHolderLabel
+    topAnchor = textField == idTextField ? idTextFieldPlaceHolderTopAnchor : passwordTextFieldPlaceHolderTopAnchor
+    
+    guard let _placeHolderLabel = placeHolderLabel,
+          let _topAnchor = topAnchor else { return }
+    
+    updateTextFieldHolder(_placeHolderLabel, fontSize: 12, _topAnchor, constant: 5)
+  }
+  
+  
+  /// textField 입력 종료 시 placeHolder Label , textFieldBorder 세팅
+  /// - Parameter textField: 해당 textField
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    updateTextFieldBorder(textField, borderWidth: 1, borderColor: UIColor.lightGray.cgColor)
+    
+    if textField == idTextField && textField.text?.isEmpty == true {
+      updateTextFieldHolder(idTextFieldPlaceHolderLabel, idTextFieldPlaceHolderTopAnchor)
+    }else if textField == passwordTextField && textField.text?.isEmpty == true{
+      updateTextFieldHolder(passwordTextFieldPlaceHolderLabel, passwordTextFieldPlaceHolderTopAnchor)
+    }
+  }
+  
+  
+  /// TextField의 Border조절
+  /// - Parameters:
+  ///   - textField: 해당 textField
+  ///   - borderWidth: border의 width
+  ///   - borderColor: border의 color
+  func updateTextFieldBorder(
+    _ textField: UITextField,
+    borderWidth: CGFloat = 4,
+    borderColor: CGColor = UIColor.systemGreen.cgColor
+  ){
+    textField.layer.borderWidth = borderWidth
+    textField.layer.borderColor = borderColor
+    textFieldUnderLine.backgroundColor = .systemGray6
+  }
+  
+  /// TextField의 placeHolder  Label 동적 조절
+  /// - Parameters:
+  ///   - placeHdolerLabel: placeHolder로 사용할 라벨
+  ///   - fontSize: 라벨의 폰트사이즈
+  ///   - topAnchor: 라벨의 topAnchor(TextField로 부터)
+  ///   - constant:topAnchor 수치
+  func updateTextFieldHolder(
+    _ placeHdolerLabel: UILabel,
+    fontSize: CGFloat = 17,
+    _ topAnchor: NSLayoutConstraint,
+    constant: CGFloat = 20
+  ){
+    placeHdolerLabel.font = .systemFont(ofSize: fontSize)
+    topAnchor.constant = constant
+    UIView.animate(withDuration: 0.3) {
+      self.layoutIfNeeded()
+    }
+  }
+}
+
--- NaverLogin/Extension/UITextField+Ext.swift
+++ NaverLogin/Extension/UITextField+Ext.swift
@@ -8,18 +8,18 @@
 import UIKit
 
 extension UITextField {
+  
   /// textField의 특정 모서리 둥글게 만들기
   /// - Parameters:
   ///   - textField: 해당 textField
   ///   - leftCorner: 왼쪽 코너
   ///   - rightCorner: 오른쪽 코너
   func setupTextFieldCornerRadius(
-    _ textField: UITextField,
     cornerRadious: CGFloat = 8,
     leftCorner: CACornerMask,
     rightCorner: CACornerMask
   ){
-    textField.layer.cornerRadius = cornerRadious
-    textField.layer.maskedCorners = CACornerMask(arrayLiteral: leftCorner, rightCorner)
+    self.layer.cornerRadius = cornerRadious
+    self.layer.maskedCorners = CACornerMask(arrayLiteral: leftCorner, rightCorner)
   }
 }
