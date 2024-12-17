d06f0d8f2c7ccc3635a9eb31d158129a438870a2
rrheon <yongheon0806@gmail.com>
2024-12-17 17:25:54 +0900

    Initial Commit

---
+++ week-10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week-10
+//
+//  Created by 최용헌 on 12/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
